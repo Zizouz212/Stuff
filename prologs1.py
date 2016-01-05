d2ff613350988bc689d9a4d616297d1da2490ef4
Zizouz212 <Zizouz212@users.noreply.github.com>
2016-01-05 16:11:25 -0500

    Update prologs1.py

--- prologs1.py
+++ prologs1.py
@@ -1,4 +1,30 @@
 #prology1.py as seen in https://sites.google.com/site/prologsite/prolog-problems/1
 
-def get_last(l):
+def get_last(l): #1.01
+  return l[len(l)-1]
+  
+def get_nth(l, index): #1.02
+  return l[index-1]
+  
+def get_size(l):
+  return len(l)
+  
+def reverse(l):
+  return l[::-1]
+  
+def is_palindrome(;):
+  return l == l[::-1]
+  
+def flatten(l):
+  out = []
+  for i in out:
+    if isinstance(i, (list, tuple, set)):
+      for j in i:
+        out.append(j)
+    out.append(i)
+  return out
+  
+def unique_list(l):
+  return list(set(l))
+  
 
